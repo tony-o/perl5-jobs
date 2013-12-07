5cb26501e05cbc7cdf716ec8a3506bb4a6c44725
tonyo <tonyo@rightclickblog.com>
2013-12-07 10:31:20 -0800

    trying more daemon shit

--- daemoner.pl
+++ daemoner.pl
@@ -8,12 +8,13 @@ my $pid;
 sub start {
   defined($pid = fork) or die 'unable to fork: ' . $!;
   if ( $pid > 0 ) { #git check
+    setpgrp $pid, -9;
     my $checker = EV::timer 0, 30, sub {
       my $status = qx{git fetch origin 2>&1}; 
       say "{Status: $status}";
       if ( $status ne '' ) {
         say '{Killing childe}';
-        kill SIGTERM, $pid;
+        kill -9, $pid;
         while (waitpid($pid, 0) != 0) { }
         say '{Pulling changes}';
         qx<git pull>;
