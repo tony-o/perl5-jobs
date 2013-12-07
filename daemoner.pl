52f317debb026f1475181618f3134f71a04299de
Tony O'Dell <tony.odell@live.com>
2013-12-07 09:30:43 -0800

    test daemon

--- daemoner.pl
+++ daemoner.pl
@@ -10,13 +10,17 @@ if ($pid) { #git check
   my $checker = EV::timer 0, 30, sub {
     my $status = qx<git pull>; 
     chomp $status; 
+    say "{Status: $status}";
     if ( $status ne 'Already up-to-date.' ) {
+      say '{Killing childe}';
       kill $pid;
       waitpid $pid, 0;
+      say '{Pulling changes}';
       qx<git pull>;
       defined($pid = fork) or die 'unable to fork: ' . $!;
     }
   };
 } else {
+  say '{Starting server..}';
   system('./start.sh'); 
 }
