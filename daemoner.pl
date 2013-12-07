0f34320ddd9bac85185283d47e04ff2b5a24d796
tonyo <tonyo@rightclickblog.com>
2013-12-07 09:22:07 -0800

    adding a daemon to update server

---
+++ daemoner.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl 
+
+use v5.16; 
+use EV;
+
+my $pid;
+defined($pid = fork) or die 'unable to fork: ' . $!;
+
+if ($pid) { #git check
+  my $checker = EV::timer 0, 30, sub {
+    my $status = qx<git pull>; 
+    chomp $status; 
+    if ( $status ne 'Already up-to-date.' ) {
+      kill $pid;
+      waitpid $pid;
+      qx<git pull>;
+      defined($pid = fork) or die 'unable to fork: ' . $!;
+    }
+  };
+} else {
+  system('./start.sh'); 
+}
