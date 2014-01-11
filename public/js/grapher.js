3feaef8296b560658b6f5ca8a7e8a6e238f8acc2
Tony O'Dell <tony.odell@live.com>
2014-01-10 16:59:54 -0800

    finished graphing example

--- public/js/grapher.js
+++ public/js/grapher.js
@@ -1,3 +1,4 @@
+var index = 0;
 $("div[role='graph']").each(function(){
   var fix = function(arg){
     var g = [];
@@ -13,7 +14,8 @@ $("div[role='graph']").each(function(){
                 }
                 this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
                 this.bar.animate({transform:"s1,1.35"},400,"elastic");
-                console.log(this.bar.zzindex);
+                var d = $("div[zz='" + this.bar[0].zzindex + "']");
+                $(d).css("font-weight","bold");
             },
             fout = function () {
                 if ((this.bar.value || "0") === "0") {
@@ -21,6 +23,8 @@ $("div[role='graph']").each(function(){
                 }
                 this.flag.animate({opacity: 0}, 300, function () {this.remove();});
                 this.bar.animate({transform:"s1,1.15"},400,"elastic");
+                var d = $("div[zz='" + this.bar[0].zzindex + "']");
+                $(d).css("font-weight","normal");
             };
   var type = $(this).attr("type");
   var custom_colors =[
@@ -41,16 +45,16 @@ $("div[role='graph']").each(function(){
     data = [[55,20,13,32,5,1]];
     var rg = r.hbarchart(0, 20, 300, 150, data).hover(fin,fout).transform("s1,1.15");
     //set colors manually
-    var index = 0;
     var colorize2 = function(){ $(this).css("color", custom_colors[index++ % custom_colors.length]); };
     for(var i in rg[1]){
       try{
         rg[1][i].bar[0].zzindex = i;
         i = rg[1][i].bar[0];
         $(i).attr("fill", custom_colors[index % custom_colors.length]);
-        var d = $("#" + $(this).attr("id") + "legend div:nth-child(" + (1+index) + ")");
-        $(d).first().css("color", index++ % custom_colors.length);
-      }catch(e){ console.log(e); }
+        var d = $("#" + $(this).attr("id") + "legend > div:nth-child(" + (1+index) + ")")[0];
+        $(d).css("color", custom_colors[index++ % custom_colors.length]);
+        $(d).attr("zz", index-1);
+      }catch(e){ }
     }
   };
 });
