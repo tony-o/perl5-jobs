e5cbffbd6beb535fb1eac2039a557380f29fda8e
Tony O'Dell <tony.odell@live.com>
2014-03-11 10:37:13 -0700

    added config::general to start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cpanm     Mojolicious Plack DBD::Pg DBIx::Class::Schema Email::Address Mojo::Server::Hypnotoad Mojolicious::Plugin::Authentication LWP::Simple LWP
+cpanm     Config::General Mojolicious Plack DBD::Pg DBIx::Class::Schema Email::Address Mojo::Server::Hypnotoad Mojolicious::Plugin::Authentication LWP::Simple LWP
 git       pull
 hypnotoad -f script/app
