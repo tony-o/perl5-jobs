e35edb10064605eeb2383db4d2b3101df0499b13
Tony O'Dell <tony.odell@live.com>
2014-03-20 12:27:55 -0700

    added video upload functions

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cpanm     Config::General Mojolicious Plack DBD::Pg DBIx::Class::Schema Email::Address Mojo::Server::Hypnotoad Mojolicious::Plugin::Authentication LWP::Simple LWP Mojolicious::Plugin::Human JSON::Tiny XML::Simple IO::Socket::SSL
+cpanm     Config::General Mojolicious Plack DBD::Pg DBIx::Class::Schema Email::Address Mojo::Server::Hypnotoad Mojolicious::Plugin::Authentication LWP::Simple LWP Mojolicious::Plugin::Human JSON::Tiny XML::Simple IO::Socket::SSL Digest::Adler32::XS
 git       pull
 hypnotoad -f script/app
